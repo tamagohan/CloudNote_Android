448135184383349f1ecfe9b2527da8c6b6dd2e92
masaki_mac <fantastipo23@gmai.com>
2012-11-25 11:25:59 +0900

    enable to post request for login

--- gen/com/tamagohan/cloudnote_android/R.java
+++ gen/com/tamagohan/cloudnote_android/R.java
@@ -15,7 +15,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button_login=0x7f070002;
+        public static final int menu_settings=0x7f070003;
+        public static final int text_login=0x7f070000;
+        public static final int text_password=0x7f070001;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -26,6 +29,10 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int label_login=0x7f040005;
+        public static final int label_password=0x7f040006;
+        public static final int login_button=0x7f040007;
+        public static final int login_title=0x7f040004;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_login=0x7f040003;
     }
--- src/com/tamagohan/cloudnote_android/LoginActivity.java
+++ src/com/tamagohan/cloudnote_android/LoginActivity.java
@@ -1,16 +1,84 @@
 package com.tamagohan.cloudnote_android;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+@SuppressLint("NewApi")
 public class LoginActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.v("EXAMPLE", "onCreate was called.");
+        setContentView(R.layout.activity_login);
+        Button buttonLogin = (Button) findViewById(R.id.button_login);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+    		public void onClick(View view) {
+    			EditText textLogin    = (EditText) findViewById(R.id.text_login);
+    			EditText textPassword = (EditText) findViewById(R.id.text_password);
+    			String login    = textLogin.getText().toString();
+    		    String password = textPassword.getText().toString();
+    			
+				try {
+					StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+					Log.v("EXAMPLE", "parepare for post");
+					HttpPost method = new HttpPost( "http://10.0.2.2:3000/user_sessions/create_api" );
+					DefaultHttpClient client = new DefaultHttpClient();
+					Log.v("EXAMPLE", "http client created.");
+    				             
+					// POST ÉfÅ[É^ÇÃê›íË
+					Log.v("EXAMPLE", "start to create post data.");
+					String params = "login=" + login + "&password=" + password;
+					StringEntity paramEntity;
+					paramEntity = new StringEntity( params );
+
+					paramEntity.setChunked( false );
+					paramEntity.setContentType( "application/x-www-form-urlencoded" );
+					Log.v("EXAMPLE", "post data created.");
+    				             
+					method.setEntity( paramEntity );
+					Log.v("EXAMPLE", "params is set");
+    			
+					HttpResponse response = client.execute( method );
+					Log.v("EXAMPLE", "post is executed.");
+					int status = response.getStatusLine().getStatusCode();
+					Log.v("EXAMPLE", Integer.toString(status));
+				} catch (UnsupportedEncodingException e) {
+					Log.v("EXAMPLE", "UnsupportedEncodingException");
+					e.printStackTrace();
+				} catch (ClientProtocolException e) {
+					Log.v("EXAMPLE", "ClientProtocolException");
+					e.printStackTrace();
+				} catch (IOException e) {
+					Log.v("EXAMPLE", "IOException");
+					e.printStackTrace();
+				}
+    		}
+    	});
     }
+    
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
