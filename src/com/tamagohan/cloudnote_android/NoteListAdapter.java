051359a1ac5caa2c61816ca015a11fe5f975a78d
masaki_mac <fantastipo23@gmai.com>
2012-12-09 12:52:42 +0900

    customize note list view

--- src/com/tamagohan/cloudnote_android/NoteListAdapter.java
+++ src/com/tamagohan/cloudnote_android/NoteListAdapter.java
@@ -31,12 +31,17 @@ public class NoteListAdapter extends ArrayAdapter {
     	if (view == null) {  
     		// 受け取ったビューがnullなら新しくビューを生成  
     		view = inflater.inflate(R.layout.note_row, null);  
+    		
+    		// 背景画像をセットする
+    		view.setBackgroundResource(R.drawable.back);
 
     		// 表示すべきデータの取得  
     		ArrayList<String> item = (ArrayList<String>) items.get(position);
     		if (item != null) {  
     			TextView title = (TextView)view.findViewById(R.id.title);
     			TextView body  = (TextView)view.findViewById(R.id.body);
+    			
+    			title.setTypeface(Typeface.DEFAULT_BOLD);
       
     			// titleをビューにセット  
     			if (title != null) {  
