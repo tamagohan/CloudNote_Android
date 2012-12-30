46c6afe3664edcc4f4a4391fa813b3d44a74fa24
masaki_mac <fantastipo23@gmai.com>
2012-12-30 16:59:02 +0900

    enable to load more notes

--- src/com/tamagohan/cloudnote_android/NoteListActivity.java
+++ src/com/tamagohan/cloudnote_android/NoteListActivity.java
@@ -27,10 +27,12 @@ import android.widget.Toast;
 
 
 public class NoteListActivity extends ListActivity {
-	private JSONArray _jsonArr  = null;
-	private Context context     = this;
-	Bundle extras               = null;
-	AlertDialog.Builder builder = null;
+	private JSONArray _jsonArr   = null;
+	private Context context      = this;
+	Bundle extras                = null;
+	AlertDialog.Builder builder  = null;
+	NoteListAdapter adapter      = null;
+	DefaultHttpClient httpClient = null;
 
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,13 +48,14 @@ public class NoteListActivity extends ListActivity {
         
         final Map<String, String> params = new HashMap<String, String>();
         params.put("page", "1");
-	    DefaultHttpClient httpClient = ((MyCloudNote) this.getApplication()).getHttpClient();
+	    // DefaultHttpClient httpClient = ((MyCloudNote) this.getApplication()).getHttpClient();
+        httpClient = ((MyCloudNote) this.getApplication()).getHttpClient();
 	    Log.d("tmp", "note list activity start");
 	    exec_get("notes", params, httpClient);
 	}
 	
     // GET通信を実行
-	private void exec_get(String url, Map<String, String> params, DefaultHttpClient httpClient) {
+	private void exec_get(String url, Map<String, String> params, final DefaultHttpClient httpClient) {
 		
 
       // 非同期タスクを定義
@@ -80,14 +83,18 @@ public class NoteListActivity extends ListActivity {
         		  Log.d("tmp", "view start");
         	      ArrayList<ArrayList> list = jsonArrayToArrayList(_jsonArr);
         	      Log.d("tmp", "json transfer is finish");
-        	  	  NoteListAdapter adapter = new NoteListAdapter(context, R.layout.note_row, list);  
-        	  	  Log.d("tmp", "create adapter finish");
+        	      // final NoteListAdapter adapter = new NoteListAdapter(context, R.layout.note_row, list);
+        	      adapter = new NoteListAdapter(context, R.layout.note_row, list);
+        	      Log.d("tmp", "create adapter finish");
         	  	  ListView lv = getListView();
         	  	  LayoutInflater inflater = getLayoutInflater();
         	  	  ViewGroup header = (ViewGroup)inflater.inflate(R.layout.note_list_header, lv, false);
+        	  	  ViewGroup footer = (ViewGroup)inflater.inflate(R.layout.note_list_footer, lv, false);
         	  	  lv.addHeaderView(header, null, false);
-        		  setListAdapter(adapter);
-        		  
+                  lv.addFooterView(footer, null, false);
+                  Log.d("tmp", "first adapter");
+                  setListAdapter(adapter);
+        		          		  
         		  // 新規作成ボタン
         		  Button buttonNew = (Button) findViewById(R.id.button_new);
         		  buttonNew.setOnClickListener(new View.OnClickListener() {
@@ -99,12 +106,30 @@ public class NoteListActivity extends ListActivity {
       	   		  });
         	  	  
         	  	  // ログアウトボタン
-        	      Button buttonLogout = (Button) findViewById(R.id.logout);
+        		  Button buttonLogout = (Button) findViewById(R.id.logout);
         	      buttonLogout.setOnClickListener(new View.OnClickListener() {
       	    		  public void onClick(View view) {
       	    			  Intent intent = new Intent(NoteListActivity.this, LoginActivity.class);
       	    			  view.getContext().startActivity(intent);
       	   		      }
+      	   		  });
+        	      
+        	      // ノートの追加読み込みボタン
+        	      Button buttonMoreNotes = (Button) findViewById(R.id.button_more_notes);
+        	      buttonMoreNotes.setOnClickListener(new View.OnClickListener() {
+      	    		  public void onClick(View view) {
+      	    			  Log.d("tmp", "more notes button is clicked.");
+      	    			  final Map<String, String> params = new HashMap<String, String>();
+      	    	          params.put("page", "2");
+      	    			  get_more_notes("notes", params, httpClient);
+      	    			  /*
+      	    			  ArrayList<String> addtionalItem = new ArrayList<String>();
+      	    			  addtionalItem.clear();
+      	    			  addtionalItem.add("test_android");
+      	    			  addtionalItem.add("test_android");
+      	    			  addtionalItem.add("33");
+      	        		  adapter.add(addtionalItem);*/
+      	   			  }
       	   		  });
         	      
         	      // 結果通知ダイアログ
@@ -145,6 +170,80 @@ public class NoteListActivity extends ListActivity {
       // タスクを開始
       task.execute();
     }
+	
+	private void get_more_notes(String url, Map<String, String> params, DefaultHttpClient httpClient) {
+		// 非同期タスクを定義
+		HttpGetTask task = new HttpGetTask(
+				this,
+				Constants.SERVER_URL + url,
+
+				// タスク完了時に呼ばれるUIのハンドラ
+				new HttpPostHandler(){
+
+					@Override
+					public void onPostCompleted(String response, Integer status) {
+						// 成功ならばノートのリスト画面へ遷移
+						Log.v("EXAMPLE", "post success");
+						Log.v("EXAMPLE", response);
+						try {
+							Log.d("tmp", "parse start");
+							_jsonArr = new JSONArray(response);
+							Log.d("tmp", "parse success");
+							for (int i = 0; i < _jsonArr.length(); i++) {
+								JSONObject jsonObject = _jsonArr.getJSONObject(i);
+								Log.d("look",jsonObject.getString("title"));
+								Log.d("look",jsonObject.getString("body"));
+							}
+							Log.d("tmp", "view start");
+							ArrayList<ArrayList> items = jsonArrayToArrayList(_jsonArr);
+							for (int i = 0; i < items.size(); i++) {
+								Log.d("tmp", "set item.");
+								Log.d("look",(String) items.get(i).get(0));
+								adapter.add(items.get(i));
+				        	}
+							adapter.debug_items();
+							Log.d("tmp", "json transfer is finish");
+							
+
+	        	      
+	        	      // 結果通知ダイアログ
+	        	      if(extras != null && extras.getString("MESSAGE") != null){
+	        			  Log.d("tmp", "notification!!!!");
+	        			  builder.setMessage(extras.getString("MESSAGE"));
+	      				  builder.create();
+	      				  builder.show();
+	        		  }
+	        	  	  Log.d("tmp", "aaaaaaaaa------------");
+	        	  	
+	        	  } catch (JSONException e) {
+	        		  Log.d("tmp", "parse fail");
+	        		  // TODO 自動生成された catch ブロック
+	        		  e.printStackTrace();
+	        	  }
+	          }
+
+	          @Override
+	          public void onPostFailed(String response, Integer status) {
+	        	  Log.v("EXAMPLE", Integer.toString(status));
+	        	  Log.d("http request error", response);
+	        	  String err_msg = "通信エラーが発生しました。";
+	        	  if (status == 403){
+	        		  err_msg = "ログインに失敗しました。\nログインIDまたはパスワードが間違っています。";
+	        	  }
+	            Toast.makeText(
+	              getApplicationContext(), 
+	              err_msg, 
+	              Toast.LENGTH_LONG
+	            ).show();
+	          }
+	        },
+	        httpClient
+	      );
+
+	      task.setParamsToUrl( params );
+	      // タスクを開始
+	      task.execute();
+	    }
 	
 	public ArrayList<ArrayList> jsonArrayToArrayList(JSONArray ja)
     {
--- src/com/tamagohan/cloudnote_android/NoteListAdapter.java
+++ src/com/tamagohan/cloudnote_android/NoteListAdapter.java
@@ -17,7 +17,7 @@ import android.widget.TextView;
 
 public class NoteListAdapter extends ArrayAdapter {  
       
-    private ArrayList items;  
+    public ArrayList items;  
     private LayoutInflater inflater;  
       
     public NoteListAdapter(Context httpPostHandler, int textViewResourceId,  ArrayList items) {  
@@ -28,52 +28,57 @@ public class NoteListAdapter extends ArrayAdapter {
     			.getSystemService(Context.LAYOUT_INFLATER_SERVICE);  
     }
     
-          
+    public void debug_items(){
+    	for (int i = 0; i < items.size(); i++) {
+    		ArrayList tmp_item = (ArrayList) items.get(i);
+    		Log.d("tmp", (String) tmp_item.get(0));
+    	}
+    }
+     
     @Override  
     public View getView(int position, View convertView, ViewGroup parent) {
     	Log.d("tmp", "ddddddddd------------");
+    	Log.d("tmp", Integer.toString(position));
     	// ビューを受け取る  
     	View view = convertView;  
-    	if (view == null) {
-    		Log.d("tmp", "eeeeeeee------------");
-    		// 受け取ったビューがnullなら新しくビューを生成  
-    		view = inflater.inflate(R.layout.note_row, null);  
+    	Log.d("tmp", "eeeeeeee------------");
+    	// 受け取ったビューがnullなら新しくビューを生成  
+    	view = inflater.inflate(R.layout.note_row, null);  
     		
-    		// 背景画像をセットする
-    		view.setBackgroundResource(R.drawable.back_ground);
+    	// 背景画像をセットする
+    	view.setBackgroundResource(R.drawable.back_ground);
 
-    		// 表示すべきデータの取得  
-    		final ArrayList<String> item = (ArrayList<String>) items.get(position);
-    		if (item != null) {  
-    			TextView title   = (TextView)view.findViewById(R.id.title);
-    			TextView body    = (TextView)view.findViewById(R.id.body);
-    			TextView id      = (TextView)view.findViewById(R.id.note_id);
-    			ImageView image  = (ImageView)view.findViewById(R.id.note_icon);
+    	// 表示すべきデータの取得  
+    	final ArrayList<String> item = (ArrayList<String>) items.get(position);
+    	if (item != null) {  
+    		TextView title   = (TextView)view.findViewById(R.id.title);
+    		TextView body    = (TextView)view.findViewById(R.id.body);
+    		TextView id      = (TextView)view.findViewById(R.id.note_id);
+    		ImageView image  = (ImageView)view.findViewById(R.id.note_icon);
+
+    		title.setTypeface(Typeface.DEFAULT_BOLD);
+
+    		// titleをビューにセット  
+    		if (title != null) {  
+    			Log.d("tmp", item.get(0));
+    			title.setText(item.get(0));  
+  			}  
+      
+    		// テキストをビューにセット  
+    		if (body != null) {  
+    			body.setText(item.get(1));  
+   			}
     			
-    			title.setTypeface(Typeface.DEFAULT_BOLD);
-      
-    			// titleをビューにセット  
-    			if (title != null) {  
-    				title.setText(item.get(0));  
-    			}  
-      
-    			// テキストをビューにセット  
-    			if (body != null) {  
-    				body.setText(item.get(1));  
+    		image.setOnClickListener(new View.OnClickListener() {
+    			public void onClick(View view) {
+    				Intent intent = new Intent(view.getContext(), NoteShowActivity.class);
+    				intent.putExtra("TITLE", item.get(0));
+    				intent.putExtra("BODY",  item.get(1));
+    				intent.putExtra("ID",  item.get(2));
+    				view.getContext().startActivity(intent);
     			}
-    			
-    			image.setOnClickListener(new View.OnClickListener() {
-    	    		public void onClick(View view) {
-    	    			Intent intent = new Intent(view.getContext(), NoteShowActivity.class);
-    	    			intent.putExtra("TITLE", item.get(0));
-    	    			intent.putExtra("BODY",  item.get(1));
-    	    			intent.putExtra("ID",  item.get(2));
-    	    			view.getContext().startActivity(intent);
-    	   			}
-    	   		});
-    		}  
-    		return view;  
-    	}
-    	return view;
+    		});
+    	}  
+   		return view;  
     }
 }
